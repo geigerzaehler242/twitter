c083548a3ca6eef0dcb85ee2595f624be249b358
fm <>
2019-02-28 19:32:25 -0500

    initial commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by fernando marto on 2019-02-28.
+//  Copyright © 2019 f. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
