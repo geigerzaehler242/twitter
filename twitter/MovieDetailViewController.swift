9fe09ad1b69125969d61ac03a1e906b615128a3e
fm <>
2019-03-03 10:27:38 -0500

    add detail view when user selects a row from table

--- twitter/MovieDetailViewController.swift
+++ twitter/MovieDetailViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
 
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieOverView: UITextView!
+    
+    
+    var theMovieImage = UIImage()
+    
+    var theMovieOverview = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        movieOverView.text = theMovieOverview
+        movieImage.image = theMovieImage
+        
+        
+        UIView.animate(withDuration: 0.0, delay: 0.5, usingSpringWithDamping: 0.25, initialSpringVelocity: 5, options: .curveEaseInOut, animations: {
+            let scale = CGAffineTransform(scaleX: 0.25, y: 0.25)
+            //let rotate = CGAffineTransform(rotationAngle: 360)
+            self.movieImage.transform = scale //rotate.concatenating(scale)
+        }, completion: { (finished: Bool) in
+
+            UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.25, initialSpringVelocity: 5, options: .curveEaseInOut, animations: {
+
+                let scale = CGAffineTransform(scaleX: 1, y: 1)
+                //let rotate = CGAffineTransform(rotationAngle: 0)
+                self.movieImage.transform = scale //rotate.concatenating(scale)
+
+            }, completion: nil)
+        })
+        
+        
+        
     }
     
 
