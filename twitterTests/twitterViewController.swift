61487a6024f15f8d624d8fa4d73baee8cbcaaad5
fm <>
2019-03-03 19:38:52 -0500

    small change to test to nill out variable

--- twitterTests/twitterViewController.swift
+++ twitterTests/twitterViewController.swift
@@ -60,6 +60,7 @@ class twitterViewController: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
         tableVC = nil
+        theMovies = nil
     }
     
     func testTableViewFirstRow() {
